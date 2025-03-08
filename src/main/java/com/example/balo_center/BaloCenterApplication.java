81f1211344c1f36cf55d26150788cb95b6a652d9
Dinh-Linh <linhnd26052004@gmail.com>
2025-03-08 23:14:12 +0700

    base project

---
+++ src/main/java/com/example/balo_center/BaloCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.balo_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaloCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaloCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/balo_center/BaloCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.balo_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaloCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
