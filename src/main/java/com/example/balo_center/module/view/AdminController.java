ca2932b75a1cdb1a67acc9617510a5cba55df6f8
Dinh-Linh <linhnd26052004@gmail.com>
2025-04-08 08:27:32 +0700

    project

--- src/main/java/com/example/balo_center/module/view/AdminController.java
+++ src/main/java/com/example/balo_center/module/view/AdminController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "admin/")
+@RequestMapping(value = "view/")
 public class AdminController {
-    @GetMapping(value = "/dashboard")
+    @GetMapping(value = "admin/dashboard")
     public String dashboard(){
         return "admin/index";
     }
