fe0f51a150ca2c18f65a5faadf6c82d1394e6ec2
tun2411 <tuandang24112004@gmail.com>
2025-05-06 08:53:03 +0700

    tuan-commit

--- src/main/java/com/example/balo_center/module/view/AdminController.java
+++ src/main/java/com/example/balo_center/module/view/AdminController.java
@@ -3,16 +3,23 @@ package com.example.balo_center.module.view;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 //Simple Datatable
 @Controller
 @RequestMapping(value = "view/")
-public class AdminController {
+public class   AdminController {
     //View user
     @GetMapping(value = "admin/dashboard")
     public String dashboard(){
         return "admin/index";
     }
+
+//    @GetMapping("/admin/dashboard")
+//    public ModelAndView dashboard(){
+//        //ModelAndView modelAndView = new ModelAndView("admin/index");
+//        return new ModelAndView("admin/index");
+//    }
 
     @GetMapping(value = "admin/user/list")
     public String user(){return "admin/list_user";}
