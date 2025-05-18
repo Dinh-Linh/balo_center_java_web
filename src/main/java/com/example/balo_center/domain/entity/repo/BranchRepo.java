f9416847761b8a5aa9885d1a891b66332be25c4e
Dinh-Linh <linhnd26052004@gmail.com>
2025-05-18 12:49:04 +0700

    Product Admin

--- src/main/java/com/example/balo_center/domain/entity/repo/BranchRepo.java
+++ src/main/java/com/example/balo_center/domain/entity/repo/BranchRepo.java
@@ -3,7 +3,9 @@ package com.example.balo_center.domain.entity.repo;
 import com.example.balo_center.domain.entity.Branch;
 import com.example.balo_center.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BranchRepo extends JpaRepository<Branch, Long> {
     Branch findByBranchName(String branchName);
 }
--- src/main/java/com/example/balo_center/domain/entity/repo/CategoryRepo.java
+++ src/main/java/com/example/balo_center/domain/entity/repo/CategoryRepo.java
@@ -2,7 +2,9 @@ package com.example.balo_center.domain.entity.repo;
 
 import com.example.balo_center.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepo extends JpaRepository<Category, Long> {
     Category findByCategoryName(String name);
 }
